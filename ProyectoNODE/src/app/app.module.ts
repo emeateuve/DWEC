550ac57207c46c65be6fcbd4578f4042d0de7ecd
emeateuve <emeateuve@gmail.com>
2018-02-05 12:28:27 +0100

    proyecto node

--- ProyectoNODE/src/app/app.module.ts
+++ ProyectoNODE/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
