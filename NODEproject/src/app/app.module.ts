7a2d8698e7c5473984536aeb34953a82e45647e1
Emeateuve <emeateuve@gmail.com>
2018-02-06 18:43:48 +0100

    .

---
+++ NODEproject/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import {ChatService} from "./chat.service";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [ChatService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
